a95532a134c774ba2bb55c4c15c10443a559f00d
Sebastian Dudas <sebastian.frontend@gmail.com>
2018-03-02 20:40:39 +0100

    Added animations for messages

--- src/layout/components/messages/messages.component.ts
+++ src/layout/components/messages/messages.component.ts
@@ -3,10 +3,32 @@ import { IMessage } from '../../../data/interface/message.interface';
 import { Subscription } from 'rxjs/Subscription';
 import { MessagesService } from '../../../data/services/messages.service';
 
+// animations
+import { trigger, state, style, animate, transition, query, keyframes } from '@angular/animations';
+
 @Component({
     selector: 'app-messages',
     templateUrl: './messages.component.html',
-    styleUrls: ['./messages.component.css']
+    styleUrls: ['./messages.component.css'],
+    animations: [
+        trigger('flyInOut', [
+            state('in', style({ transform: 'translateX(0)' })),
+            transition('void => *', [
+                animate(300, keyframes([
+                    style({ opacity: 0, transform: 'translateX(-100%)', offset: 0 }),
+                    style({ opacity: 1, transform: 'translateX(15px)', offset: 0.3 }),
+                    style({ opacity: 1, transform: 'translateX(0)', offset: 1.0 })
+                ]))
+            ]),
+            transition('* => void', [
+                animate(300, keyframes([
+                    style({ opacity: 1, transform: 'translateX(0)', offset: 0 }),
+                    style({ opacity: 1, transform: 'translateX(-15px)', offset: 0.7 }),
+                    style({ opacity: 0, transform: 'translateX(100%)', offset: 1.0 })
+                ]))
+            ])
+        ])
+    ]
 })
 export class MessagesComponent implements OnInit, OnDestroy {
 
@@ -35,7 +57,7 @@ export class MessagesComponent implements OnInit, OnDestroy {
         this.subMessagesList.unsubscribe();
     }
 
-    removeMessage(msg: IMessage){
+    removeMessage(msg: IMessage) {
         this._messages.deleteMessage(msg);
     }
 }
